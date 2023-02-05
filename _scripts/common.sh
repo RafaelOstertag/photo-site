59a2097a6140b8b52eb47653deae9b998635dd1a
Rafael Ostertag <rafi@guengel.ch>
2023-02-05 09:03:35 +0100

    script to copy from staging dir

--- _scripts/common.sh
+++ _scripts/common.sh
@@ -1,5 +1,6 @@
 IMAGE_BASE=assets/images
 IMAGE_DIRS=$(find "${IMAGE_BASE}" -type d -not \( -path '*/thumbnails' -or -path '*/mediums' -or -path "${IMAGE_BASE}" \))
+STAGING_DIR=~/Pictures/photo-site-stage
 
 echo "Identified image directories"
 echo "${IMAGE_DIRS}"
---
+++ _scripts/copy-from-staging.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+. "$(dirname $0)/common.sh"
+
+get_create_date() {
+    local photo=$1
+
+    exiftool -t "${photo}" | awk 'BEGIN { FS="\t" } $1 == "Create Date" { print $2 }' | head -1
+}
+
+concat_year_and_month() {
+    local date=$1
+
+    year=$(echo "${date}" | cut -f 1 -d ':')
+    month=$(echo "${date}" | cut -f 2 -d ':')
+  
+    echo "${year}${month}"
+}
+
+for photo in "${STAGING_DIR}"/*.jpg
+do
+    create_date=$(get_create_date "${photo}")
+    year_month=$(concat_year_and_month "${create_date}")
+    target_dir="${IMAGE_BASE}/${year_month}"
+
+    cp -iv "${photo}" "${target_dir}"
+done
+
